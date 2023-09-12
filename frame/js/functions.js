b63339a179a8fef694c6d1f5ba5b7d739d302564
Gaviria15 <samuelstiven2005@gmail.com>
2023-09-12 13:16:29 -0500

    agregue mis modulos

---
+++ frame/js/functions.js
@@ -0,0 +1,317 @@
+function clickHome(){
+    let home = document.getElementById("home");
+    home.click();
+}
+function estiloHome(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+    
+    rol.style = "background-color: none";
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+
+
+function estiloRol(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+    
+    rol.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloUsuario(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    user.style = "background: #3cc3bd; color: white";
+
+    rol.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloCita(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    cita.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    rol.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloServicio(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    service.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    rol.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloCompra(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    compra.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    rol.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloExistencia(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    exist.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    rol.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloVenta(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    venta.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    rol.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloPedido(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    pedido.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    rol.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloCliente(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    client.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    rol.style = "background-color: none";
+    emple.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloEmpleado(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    emple.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    rol.style = "background-color: none";
+    prov.style = "background-color: none";
+}
+function estiloProveedor(){
+    let rol = document.getElementById("roles");
+    let user = document.getElementById("usuarios");
+    let cita = document.getElementById("citas");
+    let service = document.getElementById("servicios");
+    let compra = document.getElementById("compras");
+    let exist = document.getElementById("existencias");
+    let venta = document.getElementById("ventas");
+    let pedido = document.getElementById("pedidos");
+    let client = document.getElementById("clientes");
+    let emple = document.getElementById("empleados");
+    let prov = document.getElementById("proveedores");
+
+    prov.style = "background-color: #3cc3bd; color: white";
+
+    user.style = "background-color: none";
+    cita.style = "background-color: none";
+    service.style = "background-color: none";
+    compra.style = "background-color: none";
+    exist.style = "background-color: none";
+    venta.style = "background-color: none";
+    pedido.style = "background-color: none";
+    client.style = "background-color: none";
+    emple.style = "background-color: none";
+    rol.style = "background-color: none";
+}
